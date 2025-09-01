d2095c55f8a7ce28e3b0d26753148c541bc44fce
Mayank Sharma <mayank.sharma.dev06@gmail.com>
2025-09-01 07:55:47 +0530

    Cors issues

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://playful-selkie-bf284f.netlify.app",
+      "https://icart-ecom.netlify.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
