a7fbe040ece83f40c3a40cd456d3dfca34e69a1c
Mayank Sharma <mayank.sharma.dev06@gmail.com>
2025-09-02 08:32:32 +0530

    Again cors Problem

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://icart-ecom.netlify.app/",
+      "https://ecommerce-b7ju.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
