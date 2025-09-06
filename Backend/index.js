cd4cacb86c56da7ae04e8bf64160ccc7b259bb32
Mayank Sharma <mayank.sharma.dev06@gmail.com>
2025-09-06 08:36:00 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ecommerce-6cax.vercel.app/",
+      "https://ecommerce-6cax.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
